694f95381b761e2dea25c872f2b5bd76f75a7934
G-Bence <gemesbence007@gmail.com>
2026-09-21 18:41:33 +0200

    Aaand... IT'S DONE =)

--- src/main.ts
+++ src/main.ts
@@ -15,49 +15,93 @@ async function loadData() {
     });
 }
 
-
-await loadData()
-createTable()
-
-
 function createTable() {
     const thead = document.getElementById("thead") as HTMLElement;
     const tbody = document.getElementById("tbody") as HTMLElement;
+
+    thead.innerHTML = ""
+    tbody.innerHTML = ""
+
     const tr_head = document.createElement("tr")
-    let tr_body = document.createElement("tr")
+   /* let tr_body = document.createElement("tr")*/
 
-    const daysUsed: string[] = []
+    const daysUsed: string[] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
-    weatherList.forEach((element, index) => {
-        if (!daysUsed.includes(element.day)) {
-            const th = document.createElement("th") as HTMLElement
-            th.textContent = element.day
-            tr_head.appendChild(th)
-            daysUsed.push(element.day)
-        }
-
-        if (index % 7 == 0 || index == 0) {
-            tr_body = document.createElement("tr")
-        }
-
-
-        let td: HTMLElement 
-
-        do {
-            td = document.createElement("td") as HTMLElement
-            tr_body.appendChild(td)
-        } while (element.day == daysUsed[index])
-
-        td.textContent = (element.temperature).toString()
-        td.classList.add(element.temperature < 10 ? "cold" : element.temperature > 30 ? "warm" : "-")
-
-
-        console.log(index)
-
-        if (index % 7 == 0) {
-            tbody.appendChild(tr_body)
-        }
+    daysUsed.forEach(day => {
+        const th = document.createElement("th") as HTMLElement
+        th.textContent = day
+        tr_head.appendChild(th)
     })
 
     thead.appendChild(tr_head)
+
+    let index = 0
+    do {
+        const tr_body = document.createElement("tr")
+        daysUsed.forEach((days) => {
+            if(index >= weatherList.length){
+                return;
+            }
+            
+            let td = document.createElement("td") as HTMLElement
+
+            const weatherListElement: weatherType = weatherList[index]
+            
+            console.log(weatherList.length)
+
+            if (weatherListElement.day.toLowerCase() == days.toLowerCase()) {
+                td.textContent = (weatherListElement.temperature).toString()
+                td.classList.add(weatherListElement.temperature < 10 ? "cold" : weatherListElement.temperature > 30 ? "warm" : "-")
+                index++
+            }
+            tr_body.appendChild(td)
+        })
+        tbody.appendChild(tr_body)
+
+    } while (index < weatherList.length)
 }
+
+
+function readData(e: Event){
+    e.preventDefault()
+    const tempInput = document.getElementById("dailyTemp") as HTMLInputElement
+    const currentDay: string = new Date().toLocaleDateString("en-EN" , { weekday: 'long' }).toString()
+
+    const newData: weatherType = {day: currentDay, temperature: Number(tempInput.value)};
+
+    weatherList.push(newData)
+
+    createTable()
+}
+
+
+function exportData(e: Event){
+    e.preventDefault()
+    let json = JSON.stringify(weatherList);
+    console.log(json)
+    json = json.replaceAll('},{', "},\n{")
+
+    const textarea = document.getElementById("exportArea") as HTMLTextAreaElement;
+    textarea.value = json.toString();
+
+}
+
+
+await loadData()
+createTable()
+const dateInput = document.getElementById("dailyDate") as HTMLInputElement
+dateInput.value = new Date().toISOString().split('T')[0]
+
+const formControl = document.getElementById("mainForm") as HTMLFormElement
+
+formControl.addEventListener("submit", (e) => {
+    readData(e)
+    formControl.reset()
+    dateInput.value = new Date().toISOString().split('T')[0]
+})
+
+const exportBtn = document.getElementById("exportButton") as HTMLButtonElement
+
+exportBtn.addEventListener("click", (e)=>{
+    exportData(e)
+})
